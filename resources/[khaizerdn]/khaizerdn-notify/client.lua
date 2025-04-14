4cd59736beacefeea9d8fbc7e3966e0d87c970e8
khaizerdn <khaizerdn@gmail.com>
2025-04-14 09:05:46 +0800

    Added khaizerdn-notify

---
+++ resources/[khaizerdn]/khaizerdn-notify/client.lua
@@ -0,0 +1,43 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+-- Show Advanced Notification
+RegisterNetEvent('khaizerdn-notify:ShowAdvancedNotification')
+AddEventHandler('khaizerdn-notify:ShowAdvancedNotification', function(title, subtitle, message, icon, iconType, duration)
+    BeginTextCommandThefeedPost('TWOSTRINGS')
+    AddTextComponentSubstringPlayerName(title .. '~n~' .. subtitle)
+    AddTextComponentSubstringPlayerName(message)
+    if icon then
+        EndTextCommandThefeedPostMessagetext(icon, icon, false, iconType or 1, title, subtitle)
+    else
+        EndTextCommandThefeedPostMessagetext('CHAR_DEFAULT', 'CHAR_DEFAULT', false, 1, title, subtitle)
+    end
+    EndTextCommandThefeedPostTicker(true, false)
+    
+    PlaySoundFrontend(-1, "Menu_Accept", "Phone_SoundSet_Default", true)
+    
+    local timer = duration or 5000
+    Citizen.Wait(timer)
+    ThefeedRemoveItem()
+end)
+
+-- Show Help Notification
+RegisterNetEvent('khaizerdn-notify:ShowHelpNotification')
+AddEventHandler('khaizerdn-notify:ShowHelpNotification', function(message, duration)
+    BeginTextCommandDisplayHelp('STRING')
+    AddTextComponentSubstringPlayerName(message)
+    EndTextCommandDisplayHelp(0, false, true, duration or 5000)
+end)
+
+-- Show Basic Notification
+RegisterNetEvent('khaizerdn-notify:ShowNotification')
+AddEventHandler('khaizerdn-notify:ShowNotification', function(message, duration)
+    BeginTextCommandThefeedPost('STRING')
+    AddTextComponentSubstringPlayerName(message)
+    EndTextCommandThefeedPostTicker(true, false)
+    
+    PlaySoundFrontend(-1, "Text_Arrive_Tone", "Phone_SoundSet_Default", true)
+    
+    local timer = duration or 3000
+    Citizen.Wait(timer)
+    ThefeedRemoveItem()
+end)
---
+++ resources/[khaizerdn]/khaizerdn-notify/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'khaizerdn'
+description 'QBCore Notification System'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+dependencies {
+    'qb-core'
+}
