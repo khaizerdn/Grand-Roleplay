cf8f5a5b8c4be7a6ca6201c140f4350ad0be5fc4
khaizerdn <khaizerdn@gmail.com>
2025-04-17 17:21:32 +0800

    Tweak

--- resources/[qbx]/qbx_diving/config/client.lua
+++ resources/[qbx]/qbx_diving/config/client.lua
@@ -3,6 +3,6 @@ return {
     debugPoly = false,
     sellLocations = {
         -- Only used when not using the target
-        {coords = vec4(-1684.13, -1068.91, 13.15, 100.0), model = 'a_m_m_salton_01', zoneDimensions = vec3(3, 3, 3)}
+        {coords = vec4(-1830.19, -1266.93, 8.62, 138.02), model = 'a_m_m_salton_01', zoneDimensions = vec3(2, 2, 2)}
     }
 }
