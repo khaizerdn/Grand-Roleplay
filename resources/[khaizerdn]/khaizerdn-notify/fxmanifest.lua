5aec4c010143d0460e3ef1b35f7910ff544ce2ae
khaizerdn <khaizerdn@gmail.com>
2025-04-20 12:06:15 +0800

    Created khaizerdn-notify

---
+++ resources/[khaizerdn]/khaizerdn-notify/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'khaizerdn'
+description 'Standalone Notification System'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+exports {
+    'ShowNotification',
+    'ShowAdvancedNotification',
+    'ShowHelpNotification',
+    'ClearHelpNotification'
+}
---
+++ resources/[khaizerdn]/khaizerdn-notify/server.lua
@@ -0,0 +1,31 @@
+-- Notification exports
+exports('ShowNotification', function(source, message, duration)
+    TriggerClientEvent('khaizerdn-notify:ShowNotification', source, message, duration)
+end)
+
+exports('ShowAdvancedNotification', function(source, title, subtitle, message, icon, iconType, duration)
+    TriggerClientEvent('khaizerdn-notify:ShowAdvancedNotification', source, title, subtitle, message, icon, iconType, duration)
+end)
+
+exports('ShowHelpNotification', function(source, message, duration)
+    TriggerClientEvent('khaizerdn-notify:ShowHelpNotification', source, message, duration)
+end)
+
+exports('ClearHelpNotification', function(source)
+    TriggerClientEvent('khaizerdn-notify:ClearHelpNotification', source)
+end)
+
+-- Test command
+-- RegisterCommand('testnotify', function(source, args)
+--     local type = args[1] and args[1]:lower() or 'basic'
+    
+--     if type == 'basic' then
+--         exports['khaizerdn-notify']:ShowNotification(source, "This is a basic notification!", 3000)
+--     elseif type == 'advanced' then
+--         exports['khaizerdn-notify']:ShowAdvancedNotification(source, "Test Title", "Test Subtitle", "This is an advanced notification!", "CHAR_SOCIAL_CLUB", 1, 5000)
+--     elseif type == 'help' then
+--         exports['khaizerdn-notify']:ShowHelpNotification(source, "This is a help notification! Press ~INPUT_CONTEXT~ to interact", 5000)
+--     else
+--         TriggerClientEvent('chat:addMessage', source, { args = { '^1Error', 'Invalid notification type. Use: basic, advanced, or help' } })
+--     end
+-- end, false)
