5fac1e32d6775a7925a88d9ae287fb46d0ed2503
khaizerdn <khaizerdn@gmail.com>
2025-04-20 15:45:06 +0800

    Update: Used GTA default notification

--- resources/[ox]/ox_inventory/data/shops.lua
+++ resources/[ox]/ox_inventory/data/shops.lua
@@ -1,6 +1,6 @@
 return {
 	General = {
-		name = '247 Supermarket',
+		name = 'shop',
 		blip = {
 			id = 59, colour = 69, scale = 0.8
 		}, inventory = {
@@ -17,43 +17,43 @@ return {
 			vec3(2557.94, 382.05, 108.62),
 			vec3(373.55, 325.56, 103.56),
 		}, targets = {
-			{ loc = vec3(24.49, -1347.26, 28.5), length = 0.7, width = 0.5, heading = 269.26, minZ = 29.5, maxZ = 29.9, distance = 1.5, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(-3039.0, 584.55, 6.91), length = 0.6, width = 0.5, heading = 16.93, minZ = 7.91, maxZ = 8.31, distance = 1.5, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(-3242.26, 999.95, 11.83), length = 0.6, width = 0.6, heading = 355.51, minZ = 12.83, maxZ = 13.23, distance = 1.5, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(1727.87, 6415.23, 34.04), length = 0.6, width = 0.6, heading = 245.4, minZ = 35.04, maxZ = 35.44, distance = 1.5, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(1960.07, 3739.98, 31.34), length = 0.6, width = 0.5, heading = 299.48, minZ = 32.34, maxZ = 32.74, distance = 1.5, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(549.03, 2671.32, 41.16), length = 0.6, width = 0.5, heading = 99.11, minZ = 42.16, maxZ = 42.56, distance = 1.5, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(2678.03, 3279.44, 54.24), length = 0.6, width = 0.5, heading = 332.05, minZ = 55.24, maxZ = 55.64, distance = 1.5, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(2557.2, 380.85, 107.62), length = 0.6, width = 0.5, heading = 359.04, minZ = 108.62, maxZ = 109.02, distance = 1.5, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(372.53, 326.38, 102.57), length = 0.6, width = 0.5, heading = 259.84, minZ = 103.57, maxZ = 103.97, distance = 1.5, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(24.49, -1347.26, 28.5), length = 0.7, width = 0.5, heading = 269.26, minZ = 29.5, maxZ = 29.9, distance = 1.5, ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(-3039.0, 584.55, 6.91), length = 0.6, width = 0.5, heading = 16.93, minZ = 7.91, maxZ = 8.31, distance = 1.5, ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(-3242.26, 999.95, 11.83), length = 0.6, width = 0.6, heading = 355.51, minZ = 12.83, maxZ = 13.23, distance = 1.5, ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(1727.87, 6415.23, 34.04), length = 0.6, width = 0.6, heading = 245.4, minZ = 35.04, maxZ = 35.44, distance = 1.5, ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(1960.07, 3739.98, 31.34), length = 0.6, width = 0.5, heading = 299.48, minZ = 32.34, maxZ = 32.74, distance = 1.5, ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(549.03, 2671.32, 41.16), length = 0.6, width = 0.5, heading = 99.11, minZ = 42.16, maxZ = 42.56, distance = 1.5, ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(2678.03, 3279.44, 54.24), length = 0.6, width = 0.5, heading = 332.05, minZ = 55.24, maxZ = 55.64, distance = 1.5, ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(2557.2, 380.85, 107.62), length = 0.6, width = 0.5, heading = 359.04, minZ = 108.62, maxZ = 109.02, distance = 1.5, ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(372.53, 326.38, 102.57), length = 0.6, width = 0.5, heading = 259.84, minZ = 103.57, maxZ = 103.97, distance = 1.5, ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
 		}
 	},
 
 	LTD = {
-		name = 'LTD Store',
+		name = 'shop',
 		blip = {
 			id = 59, colour = 69, scale = 0.8
 		}, inventory = {
 			{ name = 'burger', price = 10 },
 			{ name = 'water', price = 10 }
 		}, locations = {
-			vec3(-46.66, -1757.88, 29.42),
+			vec3(-48.47, -1757.98, 29.42),
 			vec3(-706.17, -913.52, 19.22),
 			vec3(1698.17, 4922.89, 42.06),
 			vec3(-1820.22, 794.24, 138.09),
 			vec3(1164.65, -322.64, 69.21),
 			
 		}, targets = {
-			{ loc = vec3(-46.66, -1757.88, 28.42), length = 0.5, width = 0.5, heading = 54.25, minZ = 42.06, maxZ = 42.46, distance = 1.5, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(-706.17, -913.52, 18.22), length = 0.5, width = 0.5, heading = 85.21, minZ = 42.06, maxZ = 42.46, distance = 1.5, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(1698.17, 4922.89, 41.06), length = 0.5, width = 0.5, heading = 322.93, minZ = 42.06, maxZ = 42.46, distance = 1.5, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(-1820.22, 794.24, 137.09), length = 0.5, width = 0.5, heading = 137.33, minZ = 42.06, maxZ = 42.46, distance = 1.5, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(1164.65, -322.64, 68.21), length = 0.5, width = 0.5, heading = 96.99, minZ = 42.06, maxZ = 42.46, distance = 1.5, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(-46.66, -1757.88, 28.42), length = 0.5, width = 0.5, heading = 54.25, minZ = 42.06, maxZ = 42.46, distance = 1.5, ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(-706.17, -913.52, 18.22), length = 0.5, width = 0.5, heading = 85.21, minZ = 42.06, maxZ = 42.46, distance = 1.5, ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(1698.17, 4922.89, 41.06), length = 0.5, width = 0.5, heading = 322.93, minZ = 42.06, maxZ = 42.46, distance = 1.5, ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(-1820.22, 794.24, 137.09), length = 0.5, width = 0.5, heading = 137.33, minZ = 42.06, maxZ = 42.46, distance = 1.5, ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(1164.65, -322.64, 68.21), length = 0.5, width = 0.5, heading = 96.99, minZ = 42.06, maxZ = 42.46, distance = 1.5, ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
 		}
 	},
 
 	Liquor = {
-		name = 'Robs Liquor',
+		name = 'shop',
 		blip = {
 			id = 93, colour = 69, scale = 0.8
 		}, inventory = {
@@ -68,31 +68,31 @@ return {
 			vec3(1392.562, 3604.684, 34.980),
 			vec3(-1393.409, -606.624, 30.319)
 		}, targets = {
-			{ loc = vec3(1134.14, -982.51, 45.42), length = 0.5, width = 0.5, heading = 276.9, minZ = 46.4, maxZ = 46.8, distance = 1.5, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(-1221.97, -908.28, 11.33), length = 0.6, width = 0.5, heading = 32.6, minZ = 12.3, maxZ = 12.7, distance = 1.5, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(-1486.3, -378.03, 39.16), length = 0.6, width = 0.5, heading = 136.17, minZ = 40.1, maxZ = 40.5, distance = 1.5, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(-2966.45, 390.92, 14.04), length = 0.7, width = 0.5, heading = 82.55, minZ = 15.0, maxZ = 15.4, distance = 1.5, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(1165.87, 2710.77, 37.16), length = 0.6, width = 0.5, heading = 177.88, minZ = 38.1, maxZ = 38.5, distance = 1.5, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(1392.74, 3606.37, 33.98), length = 0.6, width = 0.6, heading = 198.93, minZ = 35.0, maxZ = 35.4, distance = 1.5, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' }
+			{ loc = vec3(1134.14, -982.51, 45.42), length = 0.5, width = 0.5, heading = 276.9, minZ = 46.4, maxZ = 46.8, distance = 1.5, ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(-1221.97, -908.28, 11.33), length = 0.6, width = 0.5, heading = 32.6, minZ = 12.3, maxZ = 12.7, distance = 1.5, ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(-1486.3, -378.03, 39.16), length = 0.6, width = 0.5, heading = 136.17, minZ = 40.1, maxZ = 40.5, distance = 1.5, ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(-2966.45, 390.92, 14.04), length = 0.7, width = 0.5, heading = 82.55, minZ = 15.0, maxZ = 15.4, distance = 1.5, ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(1165.87, 2710.77, 37.16), length = 0.6, width = 0.5, heading = 177.88, minZ = 38.1, maxZ = 38.5, distance = 1.5, ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(1392.74, 3606.37, 33.98), length = 0.6, width = 0.6, heading = 198.93, minZ = 35.0, maxZ = 35.4, distance = 1.5, ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' }
 		}
 	},
 
 	YouTool = {
-		name = 'YouTool',
+		name = 'tools',
 		blip = {
 			id = 402, colour = 69, scale = 0.8
 		}, inventory = {
 			{ name = 'lockpick', price = 10 }
 		}, locations = {
-			vec3(2748.0, 3473.0, 55.67),
-			vec3(342.99, -1298.26, 32.51)
+			vec3(2748.0, 3473.0, 55.67)
+			-- vec3(342.99, -1298.26, 32.51)
 		}, targets = {
-			{ loc = vec3(2747.3, 3473.08, 54.67), length = 0.6, width = 3.0, heading = 244.8, minZ = 55.0, maxZ = 56.8, distance = 3.0,  ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' }
+			{ loc = vec3(2747.3, 3473.08, 54.67), length = 0.6, width = 3.0, heading = 244.8, minZ = 55.0, maxZ = 56.8, distance = 3.0,  ped = `mp_m_shopkeep_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' }
 		}
 	},
 
 	Ammunation = {
-		name = 'Ammunation',
+		name = 'weapons',
 		blip = {
 			id = 110, colour = 69, scale = 0.8
 		}, inventory = {
@@ -111,20 +111,20 @@ return {
 			vec3(-1117.58, 2698.61, 18.55),
 			vec3(842.44, -1033.42, 28.19)
 		}, targets = {
-			{ loc = vec3(-660.91, -933.6, 20.83), length = 0.6, width = 0.5, heading = 156.29, minZ = 21.8, maxZ = 22.2, distance = 2.0, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(808.86, -2159.03, 28.62), length = 0.6, width = 0.5, heading = 340.39, minZ = 29.6, maxZ = 30.0, distance = 2.0, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(1693.18, 3761.97, 33.71), length = 0.6, width = 0.5, heading = 198.64, minZ = 34.7, maxZ = 35.1, distance = 2.0, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(-330.67, 6085.87, 30.45), length = 0.6, width = 0.5, heading = 199.27, minZ = 31.4, maxZ = 31.8, distance = 2.0, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(253.32, -51.8, 68.94), length = 0.6, width = 0.5, heading = 49.69, minZ = 69.9, maxZ = 70.3, distance = 2.0, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(23.91, -1106.0, 28.8), length = 0.6, width = 0.5, heading = 134.55, minZ = 29.8, maxZ = 30.2, distance = 2.0, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(2566.59, 292.63, 107.73), length = 0.6, width = 0.5, heading = 335.75, minZ = 108.7, maxZ = 109.1, distance = 2.0, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(-1117.97, 2700.66, 17.55), length = 0.6, width = 0.5, heading = 201.38, minZ = 18.5, maxZ = 18.9, distance = 2.0, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
-			{ loc = vec3(841.1, -1035.26, 27.19), length = 0.6, width = 0.5, heading = 340.82, minZ = 28.2, maxZ = 28.6, distance = 2.0, ped = `a_m_m_business_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' }
+			{ loc = vec3(-661.7, -933.56, 20.83), length = 0.6, width = 0.5, heading = 166.64, minZ = 21.8, maxZ = 22.2, distance = 2.0, ped = `s_m_y_ammucity_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(809.63, -2158.99, 28.62), length = 0.6, width = 0.5, heading = 349.59, minZ = 29.6, maxZ = 30.0, distance = 2.0, ped = `s_m_y_ammucity_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(1692.63, 3761.38, 33.71), length = 0.6, width = 0.5, heading = 218.04, minZ = 34.7, maxZ = 35.1, distance = 2.0, ped = `s_m_y_ammucity_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(-331.2, 6085.37, 30.45), length = 0.6, width = 0.5, heading = 214.84, minZ = 31.4, maxZ = 31.8, distance = 2.0, ped = `s_m_y_ammucity_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(253.62, -51.06, 68.94), length = 0.6, width = 0.5, heading = 59.47, minZ = 69.9, maxZ = 70.3, distance = 2.0, ped = `s_m_y_ammucity_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(23.17, -1105.65, 28.8), length = 0.6, width = 0.5, heading = 149.02, minZ = 29.8, maxZ = 30.2, distance = 2.0, ped = `s_m_y_ammucity_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(2567.29, 292.61, 107.73), length = 0.6, width = 0.5, heading = 341.57, minZ = 108.7, maxZ = 109.1, distance = 2.0, ped = `s_m_y_ammucity_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(-1118.5, 2700.13, 17.55), length = 0.6, width = 0.5, heading = 209.63, minZ = 18.5, maxZ = 18.9, distance = 2.0, ped = `s_m_y_ammucity_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' },
+			{ loc = vec3(841.8, -1035.26, 27.19), length = 0.6, width = 0.5, heading = 348.17, minZ = 28.2, maxZ = 28.6, distance = 2.0, ped = `s_m_y_ammucity_01`, scenario = 'WORLD_HUMAN_STAND_IMPATIENT' }
 		}
 	},
 
 	PoliceArmoury = {
-		name = 'Police Armoury',
+		name = 'armoury',
 		groups = shared.police,
 		blip = {
 			id = 110, colour = 84, scale = 0.8
@@ -144,7 +144,7 @@ return {
 	},
 
 	Medicine = {
-		name = 'Pharmacy',
+		name = 'pharmacy',
 		-- groups = {
 		-- 	['ambulance'] = 0
 		-- },
@@ -161,7 +161,7 @@ return {
 	},
 
 	BlackMarketArms = {
-		name = 'Black Market (Arms)',
+		name = 'weapons',
 		inventory = {
 			{ name = 'WEAPON_DAGGER', price = 5000, metadata = { registered = false	}, currency = 'black_money' },
 			{ name = 'WEAPON_CERAMICPISTOL', price = 50000, metadata = { registered = false }, currency = 'black_money' },
