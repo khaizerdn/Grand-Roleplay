7de05febc4896a157ca7c4617ab62cd136c9f0e3
khaizerdn <khaizerdn@gmail.com>
2025-05-01 09:24:52 +0800

    Changed ox_fuel to ND_Fuel

---
+++ resources/[standalone]/ND_Fuel/source/server.lua
@@ -0,0 +1,10 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+RegisterNetEvent("ND_Fuel:pay", function(amount)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    if not Player then return end
+
+    Player.Functions.RemoveMoney('bank', math.floor(amount), "Gas Station")
+    TriggerClientEvent('QBCore:Notify', src, ("Paid: $%.2f for gas."):format(amount), 'success')
+end)
