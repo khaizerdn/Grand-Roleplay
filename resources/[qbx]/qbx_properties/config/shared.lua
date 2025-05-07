1b15e8715aff68f578e1a0be295419a65e825f12
khaizerdn <khaizerdn@gmail.com>
2025-05-07 09:54:36 +0800

    Remove other apartment

--- resources/[qbx]/qbx_properties/config/shared.lua
+++ resources/[qbx]/qbx_properties/config/shared.lua
@@ -2,36 +2,36 @@ return {
     shellUndergroundOffset = 50.0,
 
     apartmentOptions = {
-        {
-            interior = 'DellPerroHeightsApt4',
-            label = 'Del Perro Heights Apt',
-            description = 'Enjoy ocean views far away from tourists and bums on Del Perro Beach.',
-            enter = vec3(-1447.35, -537.84, 34.74)
-        },
-        {
-            interior = 'DellPerroHeightsApt7',
-            label = 'Del Perro Heights Apt',
-            description = 'Luxury Del Perro Heights apartment complex! For all you voyeurs out there!',
-            enter = vec3(-1447.35, -537.84, 34.74)
-        },
-        {
-            interior = '4IntegrityWayApt28',
-            label = '4 Integrity Way Apt',
-            description = 'This is such an promosing neighborhood, you can literally see the construction from your window!',
-            enter = vec3(-59.4, -616.29, 37.36)
-        },
-        {
-            interior = '4IntegrityWayApt30',
-            label = '4 Integrity Way Apt',
-            description = 'An apartment so expansive, all your friends will immediately know how much you paid for it.',
-            enter = vec3(-47.52, -585.86, 37.95)
-        },
-        {
-            interior = 'RichardMajesticApt2',
-            label = 'Richard Majestic Apt',
-            description = 'This breathtaking luxury condo is a stone\'s throw from AKAN Records and a Sperm Donor Clinic.',
-            enter = vec3(-936.15, -378.91, 38.96)
-        },
+        -- {
+        --     interior = 'DellPerroHeightsApt4',
+        --     label = 'Del Perro Heights Apt',
+        --     description = 'Enjoy ocean views far away from tourists and bums on Del Perro Beach.',
+        --     enter = vec3(-1447.35, -537.84, 34.74)
+        -- },
+        -- {
+        --     interior = 'DellPerroHeightsApt7',
+        --     label = 'Del Perro Heights Apt',
+        --     description = 'Luxury Del Perro Heights apartment complex! For all you voyeurs out there!',
+        --     enter = vec3(-1447.35, -537.84, 34.74)
+        -- },
+        -- {
+        --     interior = '4IntegrityWayApt28',
+        --     label = '4 Integrity Way Apt',
+        --     description = 'This is such an promosing neighborhood, you can literally see the construction from your window!',
+        --     enter = vec3(-59.4, -616.29, 37.36)
+        -- },
+        -- {
+        --     interior = '4IntegrityWayApt30',
+        --     label = '4 Integrity Way Apt',
+        --     description = 'An apartment so expansive, all your friends will immediately know how much you paid for it.',
+        --     enter = vec3(-47.52, -585.86, 37.95)
+        -- },
+        -- {
+        --     interior = 'RichardMajesticApt2',
+        --     label = 'Richard Majestic Apt',
+        --     description = 'This breathtaking luxury condo is a stone\'s throw from AKAN Records and a Sperm Donor Clinic.',
+        --     enter = vec3(-936.15, -378.91, 38.96)
+        -- },
         {
             interior = 'TinselTowersApt42',
             label = 'Tinsel Towers Apt',
