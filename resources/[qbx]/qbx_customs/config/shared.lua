8fee30fb3f9d05d48d5de1a196382dd72de13323
khaizerdn <khaizerdn@gmail.com>
2025-05-07 09:35:45 +0800

    Remove other customs and change locales

--- resources/[qbx]/qbx_customs/config/shared.lua
+++ resources/[qbx]/qbx_customs/config/shared.lua
@@ -23,7 +23,7 @@ return {
         --     },
         -- Default GTA 5 Customs and Benny's Locations
         {
-            hideBlip = false,
+            hideBlip = true,
             blip = {
                 sprite = 72,
                 color = 3,
@@ -37,85 +37,85 @@ return {
                 vec3(-348.59, -139.1, 38.60),
             }
         },
-        {
-            hideBlip = false,
-            blip = {
-                sprite = 72,
-                color = 3,
-                scale = 0.8,
-                label = 'Los Santos Customs - Airport',
-            },
-            points = {
-                vec3(-1147.7, -1990.31, 13.15),
-                vec3(-1171.05, -2013.96, 13.15),
-                vec3(-1158.38, -2026.03, 13.15),
-                vec3(-1139.17, -2007.18, 13.15),
-                vec3(-1144.73, -1992.89, 13.15),
-            }
-        },
-        {
-            hideBlip = false,
-            blip = {
-                sprite = 72,
-                color = 3,
-                scale = 0.8,
-                label = 'Los Santos Customs - East',
-            },
-            points = {
-                vec3(724.93, -1092.04, 22.15),
-                vec3(738.52, -1094.83, 22.15),
-                vec3(737.36, -1064.56, 22.15),
-                vec3(724.14, -1063.71, 22.15),
-            }
-        },
-        {
-            hideBlip = false,
-            blip = {
-                sprite = 72,
-                color = 3,
-                scale = 0.8,
-                label = 'Los Santos Customs - Sandy Shores',
-            },
-            points = {
-                vec3(1172.12, 2644.76, 38.55),
-                vec3(1171.39, 2635.66, 38.55),
-                vec3(1189.77, 2636.08, 38.55),
-                vec3(1189.74, 2644.07, 38.55),
-            }
-        },
-        {
-            hideBlip = false,
-            blip = {
-                sprite = 72,
-                color = 3,
-                scale = 0.8,
-                label = 'Los Santos Customs - Paleto',
-            },
-            points = {
-                vec3(115.55, 6625.32, 31.75),
-                vec3(109.19, 6631.69, 31.75),
-                vec3(97.39, 6620.02, 31.75),
-                vec3(102.72, 6613.48, 31.75),
-            }
-        },
-        {
-            hideBlip = false,
-            blip = {
-                sprite = 72,
-                color = 3,
-                scale = 0.8,
-                label = "Benny's Motorworks",
-            },
-            blipColor = 5,
-            points = {
-                vec3(-203.55, -1311.26, 30.85),
-                vec3(-228.06, -1319.24, 30.85),
-                vec3(-228.25, -1334.25, 30.85),
-                vec3(-214.18, -1341.38, 30.85),
-                vec3(-195.42, -1321.19, 30.85),
-                vec3(-195.26, -1314.11, 30.85),
-            }
-        }
+        -- {
+        --     hideBlip = false,
+        --     blip = {
+        --         sprite = 72,
+        --         color = 3,
+        --         scale = 0.8,
+        --         label = 'Los Santos Customs - Airport',
+        --     },
+        --     points = {
+        --         vec3(-1147.7, -1990.31, 13.15),
+        --         vec3(-1171.05, -2013.96, 13.15),
+        --         vec3(-1158.38, -2026.03, 13.15),
+        --         vec3(-1139.17, -2007.18, 13.15),
+        --         vec3(-1144.73, -1992.89, 13.15),
+        --     }
+        -- },
+        -- {
+        --     hideBlip = false,
+        --     blip = {
+        --         sprite = 72,
+        --         color = 3,
+        --         scale = 0.8,
+        --         label = 'Los Santos Customs - East',
+        --     },
+        --     points = {
+        --         vec3(724.93, -1092.04, 22.15),
+        --         vec3(738.52, -1094.83, 22.15),
+        --         vec3(737.36, -1064.56, 22.15),
+        --         vec3(724.14, -1063.71, 22.15),
+        --     }
+        -- },
+        -- {
+        --     hideBlip = false,
+        --     blip = {
+        --         sprite = 72,
+        --         color = 3,
+        --         scale = 0.8,
+        --         label = 'Los Santos Customs - Sandy Shores',
+        --     },
+        --     points = {
+        --         vec3(1172.12, 2644.76, 38.55),
+        --         vec3(1171.39, 2635.66, 38.55),
+        --         vec3(1189.77, 2636.08, 38.55),
+        --         vec3(1189.74, 2644.07, 38.55),
+        --     }
+        -- },
+        -- {
+        --     hideBlip = false,
+        --     blip = {
+        --         sprite = 72,
+        --         color = 3,
+        --         scale = 0.8,
+        --         label = 'Los Santos Customs - Paleto',
+        --     },
+        --     points = {
+        --         vec3(115.55, 6625.32, 31.75),
+        --         vec3(109.19, 6631.69, 31.75),
+        --         vec3(97.39, 6620.02, 31.75),
+        --         vec3(102.72, 6613.48, 31.75),
+        --     }
+        -- },
+        -- {
+        --     hideBlip = false,
+        --     blip = {
+        --         sprite = 72,
+        --         color = 3,
+        --         scale = 0.8,
+        --         label = "Benny's Motorworks",
+        --     },
+        --     blipColor = 5,
+        --     points = {
+        --         vec3(-203.55, -1311.26, 30.85),
+        --         vec3(-228.06, -1319.24, 30.85),
+        --         vec3(-228.25, -1334.25, 30.85),
+        --         vec3(-214.18, -1341.38, 30.85),
+        --         vec3(-195.42, -1321.19, 30.85),
+        --         vec3(-195.26, -1314.11, 30.85),
+        --     }
+        -- }
     },
 
     prices = {
