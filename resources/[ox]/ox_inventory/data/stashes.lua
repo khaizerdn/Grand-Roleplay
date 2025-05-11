25fa5b505ddc014b235257f73288b8eb036dbf22
khaizerdn <khaizerdn@gmail.com>
2025-05-11 16:31:38 +0800

    Added La Fuente Blanca Inventories

--- resources/[ox]/ox_inventory/data/stashes.lua
+++ resources/[ox]/ox_inventory/data/stashes.lua
@@ -36,4 +36,73 @@ return {
 	-- 	weight = 70000,
 	-- 	groups = {['ambulance'] = 0}
 	-- },
+
+	{ -- La Fuente Blanca Inventory 1
+		coords = vec3(1404.82, 1139.71, 109.75),
+		target = {
+			loc = vec3(1404.82, 1139.71, 109.75),
+			length = 1.2,
+			width = 5.6,
+			heading = 0,
+			minZ = 29.49,
+			maxZ = 32.09,
+			label = 'Inventory'
+		},
+		name = 'lfb_inventory1',
+		label = 'Inventory',
+		owner = false,
+		slots = 70,
+		weight = 70000
+	},
+	{ -- La Fuente Blanca Inventory 2
+		coords = vec3(1399.02, 1144.9, 109.67),
+		target = {
+			loc = vec3(1399.02, 1144.9, 109.67),
+			length = 1.2,
+			width = 5.6,
+			heading = 0,
+			minZ = 29.49,
+			maxZ = 32.09,
+			label = 'Inventory'
+		},
+		name = 'lfb_inventory2',
+		label = 'Inventory',
+		owner = false,
+		slots = 70,
+		weight = 70000
+	},
+	{ -- La Fuente Blanca Inventory 3
+		coords = vec3(1395.09, 1135.52, 109.75),
+		target = {
+			loc = vec3(1395.09, 1135.52, 109.75),
+			length = 1.2,
+			width = 5.6,
+			heading = 0,
+			minZ = 29.49,
+			maxZ = 32.09,
+			label = 'Inventory'
+		},
+		name = 'lfb_inventory3',
+		label = 'Inventory',
+		owner = false,
+		slots = 70,
+		weight = 70000
+	},
+	{ -- La Fuente Blanca Inventory 4
+		coords = vec3(1394.65, 1150.02, 114.33),
+		target = {
+			loc = vec3(1394.65, 1150.02, 114.33),
+			length = 1.2,
+			width = 5.6,
+			heading = 0,
+			minZ = 29.49,
+			maxZ = 32.09,
+			label = 'Inventory'
+		},
+		name = 'lfb_inventory4',
+		label = 'Inventory',
+		owner = false,
+		slots = 70,
+		weight = 70000
+	},
 }
