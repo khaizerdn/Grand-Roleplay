96ebc39166c11d9491ea990c707d8b56064ba100
khaizerdn <khaizerdn@gmail.com>
2025-05-11 13:00:06 +0800

    Lower the density of parked vehicles
    Trying to prevent la fuente vehicle spawning

--- resources/[qbx]/qbx_density/config/client.lua
+++ resources/[qbx]/qbx_density/config/client.lua
@@ -3,7 +3,7 @@ return {
     -- Minimum value is 0.0, maximum value is 1.0
     -- Value of 1.0 represents GTA Online populate rates
     -- Value of 0.0 removes all of that type
-    parked = 1.0, -- Density of parked vehicles
+    parked = 0.8, -- Density of parked vehicles
     vehicle = 1.0, -- Density of vehicles
     randomvehicles = 1.0, -- Density of random vehicles
     peds = 1.0, -- Density of random peds (civilians, etc.)
